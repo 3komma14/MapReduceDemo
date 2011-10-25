06188c159e355c7c457fc1b1d914a701524566f8
Per Ivar Seterlund <3komma14@gmail.com>
2011-10-25 15:18:46 +0200

    Some refactoring

--- MapReduce/Program.cs
+++ MapReduce/Program.cs
@@ -15,47 +15,35 @@ namespace MapReduce
             // Input reader
             var lines = File.ReadAllLines("LoremIpsum.txt");
 
-            #region Non Task Version
-
             // Map
-            var startMapNonTask = DateTime.Now;
-            var mappedResultNonTask = MapNonTask(lines);
-            var endMapNonTask = DateTime.Now;
-            Console.WriteLine("Word count = {0}", mappedResultNonTask.Count());
-            Console.WriteLine("Non Task Time: {0}", endMapNonTask.Subtract(startMapNonTask).Milliseconds);
-            Console.ReadLine();
-
-            #endregion            
-            
-            // Map
-            var startMap = DateTime.Now;
-            var mappedResult = Map(lines);
-            var endMap = DateTime.Now;
-            Console.WriteLine("Word count = {0}", mappedResult.Count());
-            Console.WriteLine("Time: {0}", endMap.Subtract(startMap).Milliseconds);
+            var mappedResult = Map(lines, GetWords);
+            Console.WriteLine("Map result: Word count = {0}", mappedResult.Count());
             Console.ReadLine();
 
             // Reduce
-            var result = Reduce(mappedResult);
+            var result = Reduce(mappedResult, GetWordCount("Lorem"));
             Console.WriteLine("Reduce result: Lorem found {0} times", result);
             Console.ReadLine();
         }
 
-        private static IEnumerable<WordItem> Map(IList<string> lines)
+
+        #region Map
+
+        private static IEnumerable<ResultItem> Map(IList<string> lines, Func<string, IEnumerable<ResultItem>> function)
         {
-            var result = new List<WordItem>();
-            var tasks = new Task<IEnumerable<WordItem>>[lines.Count];
+            var result = new List<ResultItem>();
+            var tasks = new Task<IEnumerable<ResultItem>>[lines.Count];
 
             // Split
             for (var i = 0; i < lines.Count; i++)
             {
                 var index = i;
                 var data = lines[i];
-                tasks[i] = Task.Factory.StartNew(() => PartialMap(index, data));
+                tasks[i] = Task.Factory.StartNew(() => function(data));
             }
 
             Task.WaitAll(tasks);
-            
+
             // Combine
             foreach (var task in tasks)
             {
@@ -63,32 +51,30 @@ namespace MapReduce
             }
 
             return result;
-        }        
-        
-        private static IEnumerable<WordItem> MapNonTask(IList<string> lines)
-        {
-            var result = new List<WordItem>();
-
-            // Split
-            for (var i = 0; i < lines.Count; i++)
-            {
-                result.AddRange(PartialMap(i, lines[i]));
-            }
-
-            return result;
         }
 
-        private static int Reduce(IEnumerable<WordItem> mappedResult)
+        private static IEnumerable<ResultItem> GetWords(string input)
         {
-            return mappedResult.Sum(x => string.Compare(x.Key, "Lorem") == 0 ? x.Value : 0);
-        }
-
-        private static IEnumerable<WordItem> PartialMap(int id, string input)
-        {
-            Console.WriteLine("Map(id = {0})", id);
-            Thread.Sleep(10); 
             var tokenizer = new StringTokenizer(input);
-            return tokenizer.GetTokens().Select(token => new WordItem(token, 1));
+            var words = tokenizer.GetTokens();
+            return words.Select(token => new ResultItem(token, 1));
         }
+
+        #endregion
+
+        #region Reduce
+
+        private static int Reduce(IEnumerable<ResultItem> mappedResult, Func<ResultItem, int> function)
+        {
+            return mappedResult.Sum(function);
+        }
+
+        private static Func<ResultItem, int> GetWordCount(string word)
+        {
+            return x => string.Compare(x.Word, word) == 0 ? x.Value : 0;
+        }
+
+        #endregion
+
     }
 }
---
+++ MapReduce/ResultItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MapReduce
+{
+    public class ResultItem
+    {
+        public string Word { get; set; }
+        public int Value { get; set; }
+
+        public ResultItem(string word, int value)
+        {
+            Word = word;
+            Value = value;
+        }
+    }
+}
